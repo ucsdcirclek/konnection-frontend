b53280393d91d1fcdc1e0617d5750e4c9fbf5ee3
Joseph Le <josephle@me.com>
2014-09-11 15:09:13 -0700

    #16029874660848: Modified Restangular settings and added config to load token

--- app/scripts/konnection.js
+++ app/scripts/konnection.js
@@ -2,18 +2,30 @@
 
 var app = angular.module('konnection', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router']);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'partials/main.html',
-        controller: 'MainCtrl'
-      });
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/main.html',
+      controller: 'MainCtrl'
+    });
 
-    $urlRouterProvider.otherwise('/');
-  })
+  $urlRouterProvider.otherwise('/');
+})
 ;
 
 app.run(function(Restangular, BaseUrlCalculator) {
-    Restangular.setBaseUrl('http://api.ucsdcki.org');
+  Restangular.setBaseUrl('http://api.ucsdcki.org');
+
+  Restangular.setRequestInterceptor(function(elem, operation) {
+    if (operation === "remove") {
+      return null;
+    }
+    return elem;
+  });
+
+  if (sessionStorage.token || localStorage.token) {
+    Restangular.setDefaultHeaders({'X-Auth-Token': $window.localStorage.token});
+  }
+
 });
