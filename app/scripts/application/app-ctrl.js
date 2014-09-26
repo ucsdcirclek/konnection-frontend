10d8d0111b4d61d7e2a00d6e67d0239d4ba44979
Joseph Le <josephle@me.com>
2014-09-25 23:38:46 -0700

    Added admin link and tweaked roles

--- app/scripts/application/app-ctrl.js
+++ app/scripts/application/app-ctrl.js
@@ -1,6 +1,6 @@
 app.controller('AppCtrl', ['$scope', 'USER_ROLES', 'Auth', function($scope, USER_ROLES, Auth) {
   $scope.currentUser = null;
-  $scope.userRoles = [0];
+  $scope.userRoles = USER_ROLES;
   $scope.isAuthorized = Auth.isAuthorized;
 
   $scope.setCurrentUser = function(user) {
